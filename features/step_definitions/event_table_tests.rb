77f3ef0bda0dbc8e5661e02cd64ecd880d0cf7a4
yang-421 <35973752+yang-421@users.noreply.github.com>
2018-03-04 20:20:14 -0600

    Add files via upload

---
+++ features/step_definitions/event_table_tests.rb
@@ -0,0 +1,27 @@
+Given("I land on Home screen") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am a user") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I am on http://NCSC dot com/") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see event table") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I am on http://NSCE dot com/") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("The event table is not showing") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
