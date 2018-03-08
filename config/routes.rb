5728672c125151ba0868532daca722314267f627
Tri Pham <phamminhtris@tamu.edu>
2018-03-08 17:07:20 -0600

    start from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'static_pages/login'
+  root "static_pages#login"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
