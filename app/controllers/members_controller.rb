3ff3ce347017ca93cc7dc92188dc9d22835f4ad3
jtiu.edu <jtiu.edu@gmail.com>
2018-03-12 20:56:22 +0000

    Replaced static_pages controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,14 @@
+class MembersController < ApplicationController
+    
+    def login
+    end
+    
+    def home
+    end
+    
+    def faq
+    end
+    
+    def about
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/login'
-  get 'static_pages/about'
-  get 'static_pages/faq'
-  root "static_pages#login"
+  get 'members/login'
+  get 'members/about'
+  get 'members/faq'
+  
+  root "members#login"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
