5867c251153b68e038ac4b196f2cd4bc3d76183b
Tri Pham <phamminhtris@tamu.edu>
2018-03-14 21:55:27 -0500

    Added google auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   get 'members/login'
   get 'members/about'
   get 'members/faq'
+
+  # below are routes needed for google authentication
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
   
   root "members#login"
 
