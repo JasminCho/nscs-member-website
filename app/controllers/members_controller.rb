fbac082dcc98b84a3f35c2175c6c86ed167a1e9b
jtiu.edu <jtiu.edu@gmail.com>
2018-03-26 21:47:02 +0000

    commented out admin check

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,11 +2,13 @@ class MembersController < ApplicationController
     
     def home
         @newsfeed = Newsfeed.all
+=begin
         if session[:user]
             if session[:user].admin == true
                 @admin = true
             end
         end
+=end
     end
     
     def faq
