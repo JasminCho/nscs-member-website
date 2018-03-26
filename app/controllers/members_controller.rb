d99610634350f8b52366627083d3ab3aadf30944
jtiu.edu <jtiu.edu@gmail.com>
2018-03-26 22:08:19 +0000

    Added link to new news post

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,13 +2,6 @@ class MembersController < ApplicationController
     
     def home
         @newsfeed = Newsfeed.all
-=begin
-        if session[:user]
-            if session[:user].admin == true
-                @admin = true
-            end
-        end
-=end
     end
     
     def faq
