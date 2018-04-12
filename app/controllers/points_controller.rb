066f18c4441993d8e6b16430d63c2361023b8db8
Tri Pham <phamminhtris@tamu.edu>
2018-04-11 23:03:43 -0500

    Initial version point_controller

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,28 +1,34 @@
 class PointsController < ApplicationController
   def index
-    member_email = params[:email]
-    @points = Point.where(email: member_email)
-    debugger
-
+    @member_email = params[:email]
+    @points = Point.where(email: @member_email)
   end
 
   def new
-
+    @point = Point.new
   end
 
   def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @point = Point.new(points_params)
+    if @point.save
+      redirect_to(points_index_path(:email => points_params[:email]))
+    else
+      render 'new'
+    end
   end
 
   def destroy
-
+    id = params[:id]
+    email = params[:email]
+    @point = Point.find(id)
+    @point.destroy
+    
+    redirect_to points_index_url(:email => email)
   end
+
+  private 
+  def points_params
+    params.require(:point).permit(:email, :event_name, :point)
+  end
+
 end
