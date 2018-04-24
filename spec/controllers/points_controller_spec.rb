6cc3fba87ff23d37fdbf3502ef801cfdf5f84005
Tri Pham <phamminhtris@tamu.edu>
2018-04-24 14:33:35 -0500

    Added some tests to points_controller_spec.rb

--- spec/controllers/points_controller_spec.rb
+++ spec/controllers/points_controller_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 
+RSpec.describe PointsController, type: :controller do
+  describe 'New point' do 
+    it 'receieves non-nil arguments' do 
+        post :new
+        expect(assigns(@point)).not_to be_nil
+    end
+  end
+
+  describe 'Destroy point' do
+    it 'receive correct arguments' do
+        delete :destroy, :params => {:id => 1, :email => 'email'}
+        expect(controller.params[:id]).not_to be_nil
+        exepct(controller.params[:email]).not_to be_nil
+    end  
+  end
+end
+
+
