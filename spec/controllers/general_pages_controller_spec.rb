a884d37d6c697cb3cac404011d263cf70238e6d6
Tri Pham <phamminhtris@tamu.edu>
2018-04-24 15:22:18 -0500

    Added some tests to general_pages_controller_spec.rb

--- spec/controllers/general_pages_controller_spec.rb
+++ spec/controllers/general_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GeneralPagesController, type: :controller do
-
+    describe 'GET home' do
+        it 'gets some newfeeds' do
+            get :home
+            expect(assigns(@newfeeds)).not_to be_nil
+        end
+    end
 end
