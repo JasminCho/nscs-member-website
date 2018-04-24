e4bc6f8396212d691691a717c3ec07b7bcf67dfb
user1 <gregorio_garza@tamu.edu>
2018-04-24 17:05:22 -0500

    Merging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -107,4 +107,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  #RSpec.configure do |config|
+  #  config.before(:each) do
+  #    stub_request(:any, /)
 end
